bb8af09110f4bf922501a8d2b09cccf1abdba22e
yumikeuy <mishanchik.yushkevich@gmail.com>
2025-05-30 15:56:55 +0200

    LU

--- gaussElim.m
+++ gaussElim.m
@@ -1,14 +1,18 @@
-function x = gaussElim(x)
+function [x, l] = gaussElim(x)
 % Autor: Mikhail Yushkevich.
 % Funkcja stosuje algorytm eliminacji Gaussa do macierzy x.
 
+l = zeros(size(x, 1), size(x, 2));
 for j = 1:size(x, 2)
     for i = 1:size(x, 1)
         if(i <= j)
+            if(i == j)
+                l(i, j) = 1;
+            end
             continue
         end
-            l = x(i, j) / x(j, j);
-            x(i, :) = x(i, :) - l*x(j, :);
+            l(i, j) = x(i, j) / x(j, j);
+            x(i, :) = x(i, :) - l(i, j)*x(j, :);
     end
 end
 
